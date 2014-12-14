ca315de47de70197ed9acd9ecb18a6ed01958ccc
Otávio Garcia <otavio@otavio.com.br>
2014-12-14 01:05:36 -0200

    Adding initial files

---
+++ src/main/java/br/com/otavio/jpa/javatime/PersistentYearMonth.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2014 Otávio S Garcia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package br.com.otavio.jpa.javatime;
+
+import java.sql.Date;
+import java.time.YearMonth;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * Convert a {@link YearMonth} into database column representation as {@link Date} and back again.
+ * 
+ * @author Otávio S Garcia
+ */
+@Converter(autoApply = true)
+public class PersistentYearMonth implements AttributeConverter<YearMonth, Date> {
+
+	@Override
+	public Date convertToDatabaseColumn(YearMonth value) {
+		return value == null ? null : Date.valueOf(value.atDay(1));
+	}
+
+	@Override
+	public YearMonth convertToEntityAttribute(Date value) {
+		return value == null ? null : YearMonth.from(value.toLocalDate());
+	}
+}
